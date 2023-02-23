4d662979dfde5705115b01cf8d737ef3a6f2895c
Ziru Zou <jiyucoding@yahoo.com>
2023-02-23 12:56:09 -0500

    define an instance method raise_hand() for Student class that prints out a message

--- lib/student.py
+++ lib/student.py
@@ -2,5 +2,10 @@ class Student:
     def hello(self):
         print("Hey there! I'm so excited to learn stuff.")
 
+    
+    def raise_hand(self):
+        print("Pick me!")
+
+
 class ChattyStudent(Student):
-    pass
+    
