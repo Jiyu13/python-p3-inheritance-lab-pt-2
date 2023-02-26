dce9b0b2c63791e22f00688c9670d324f4c14582
Ziru Zou <jiyucoding@yahoo.com>
2023-02-26 08:37:00 -0500

    redo

--- lib/student.py
+++ lib/student.py
@@ -8,18 +8,13 @@ class Student:
 
 
 class ChattyStudent(Student):
-
+    
     def hello(self):
         super().hello()
+        '''says a brief greeting, then tries to spoil a TV show.'''
         print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
-    
+        
 
     def raise_hand(self):
-        times = 1
-        while times < 11:
+        for _ in range(10):
             super().raise_hand()
-            times += 1
-
-    # def raise_hand(self):
-    #     for x in range(0,10):
-    #         super().raise_hand()
