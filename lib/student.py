4bc049c01eeda473561a300739962681ced1fbef
Ziru Zou <jiyucoding@yahoo.com>
2023-02-27 00:01:22 -0500

    add notes

--- lib/student.py
+++ lib/student.py
@@ -8,13 +8,15 @@ class Student:
 
 
 class ChattyStudent(Student):
-    
+
+    # inherit hello() from superclass and add customise it
     def hello(self):
         super().hello()
         '''says a brief greeting, then tries to spoil a TV show.'''
         print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
         
 
+    # overwrite raise_hand()
     def raise_hand(self):
         for _ in range(10):
             super().raise_hand()
